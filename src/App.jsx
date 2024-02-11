3c870583c56049f496f9b12ec67b400153b4295b
Bikash Das <bikashd003@gmail.com>
2024-02-11 17:31:41 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './Components/LandingPage/Home';
+import SignUp from './Components/CreateAccountPage/SignUp';
+import Profile from './Components/Profile/Profile';
+import Login from './Components/LoginPage/Login';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/create-account",
+    element: <SignUp />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+
+]);
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
